b6603f237fcc8a6d11c940938ce7aeee9753f26c
Mr-Forder <colforder@gmail.com>
2022-02-01 18:27:16 +0000

    created hero section

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import Video from "../../videos/video2.mp4";
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight,
+} from "./HeroElements";
+
+const HeroSection = () => {
+  const [hover, setHover] = useState(false);
+  const onHover = () => {
+    setHover(!hover);
+  };
+  return (
+    <HeroContainer>
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Your Money. Made Simple.</HeroH1>
+        <HeroP>
+          Connect your Ethereum wallet to send and recieve with ease.
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button to="signup" onMouseEnter={onHover} onMouseLeave={ononHover}>
+            Get Started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+};
+
+export default HeroSection;
