82d13812ec06ba850b86fefc7e4c5741362ad3ae
Mr-Forder <colforder@gmail.com>
2022-02-03 13:37:26 +0000

    nav scroll colour useEffect

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -3,19 +3,19 @@ import { Link as LinkR } from "react-router-dom";
 import { Link as LinkS } from "react-scroll";
 
 export const Nav = styled.nav`
-background: black;
-height: 80px;background;
- margin-top:-80px; 
-display: flex;
-justify-content:center;
-align-items:center;
-font-size:1rem;
-position:sticky;
-top: 0;
-z-index:10;
-@media screen and (max-width: 960px){
+  background: ${({ scrollNav }) => (scrollNav ? "#000" : "transparent")};
+  height: 80px;
+  margin-top: -80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1rem;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  @media screen and (max-width: 960px) {
     transition: 0.8s all ease;
-}
+  }
 `;
 
 export const NavBarContainer = styled.div`
